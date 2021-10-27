fe69f31425d0e110a3f6127000327314cc631414
ergz <emnlrodriguez@gmail.com>
2021-10-26 19:26:52 -0700

    creates the playing field

--- playfield/playfield.asm
+++ playfield/playfield.asm
@@ -1,39 +1,112 @@
-        processor 6502
+;;; Set the startup code, here we are simply specifying the processor type
+;;; and including some header files, as well as setting the position 
+;;; where we are starting our rom
 
-        include "../vcs.h"
-        include "../macro.h"
+    processor 6502
 
-        seg
-        org $F000
+    include "../vcs.h"
+    include "../macro.h"
+
+    seg
+    org $F000
 
 Reset:
-        CLEAN_START
+    CLEAN_START
 
-        ldx #$80
-        stx COLUBK
+;;; Color the background and playfield
 
-        lda #$1C
-        sta COLUPF
+    ldx #$80                ; load color BLUE in the x registry
+    stx COLUBK              ; and set it to the background color address
+
+    lda #$1C                ; load the color YELLOW in the a registry
+    sta COLUPF              ; and set it to the Color-Luminance Playfield address 
 
 StartFrame:
-        lda #02
-        sta VBLANK
-        sta VSYNC
 
-        REPEAT 3
-                sta WSYNC       ; the three scanlines
-        REPEND
-        lda #0
-        sta VSYNC
+;;; draw three lines of scanlines
+    lda #02                 ; load literal 2 into A (activate VBLANK)
+    sta VBLANK              
+    sta VSYNC
 
-        REPEAT 37
-                sta WSYNC
-        REPEND
-        lda #0
-        sta VBLANK              ; turn off vblank
+    REPEAT 3
+        sta WSYNC       ; the three scanlines
+    REPEND
+    lda #0                  ; turn off VSYNC
+    sta VSYNC
 
-        ldx #%00000001          ; reflect the playfield
-        stx CTRLPF
+    REPEAT 37
+        sta WSYNC
+    REPEND
+    lda #0
+    sta VBLANK              ; turn off vblank
+
+    ldx #%00000001          ; reflect the playfield
+    stx CTRLPF
+
+    ldx #0
+    stx PF0
+    stx PF1
+    stx PF2
+    REPEAT 7
+        sta WSYNC
+    REPEND
+
+;;; 192 lines
+
+;; top
+    ldx #%11100000
+    stx PF0
+    ldx #%11111111
+    stx PF1
+    stx PF2
+    REPEAT 7
+        sta WSYNC
+    REPEND
+
+;; middle
+    ldx #%00100000
+    stx PF0
+    ldx #%00000000
+    stx PF1
+    stx PF2
+    REPEAT 164
+        sta WSYNC
+    REPEND
+
+;; bottom
+    ldx #%11100000
+    stx PF0
+    ldx #%11111111
+    stx PF1
+    stx PF2
+    REPEAT 7
+        sta WSYNC
+    REPEND
+
+;; bottom is blank just like the first seven lines
+    ldx #0
+    stx PF0
+    stx PF1
+    stx PF2
+    REPEAT 7
+        sta WSYNC
+    REPEND
 
 
-        
+
+;;; Overflow ----------------------------------
+    lda #2
+    sta VBLANK
+    REPEAT 30
+        sta WSYNC
+    REPEND
+    lda #0
+    sta VBLANK
+
+    jmp StartFrame
+
+    org $fffc
+    .word Reset
+    .word Reset
+
+    
